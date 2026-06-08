39dfabf0fad2e440d0c2abd66fb03eda38ae29c5
amandagoncalvesoliveira18-prog <amanda.goncalves.oliveira18@escola.pr.gov.br>
2026-06-08 08:06:53 -0300

    js amanda agrinho

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function milhoFeliz() {
+  alert("O milho é um dos alimentos mais cultivados pelos agricultores familiares! 🌽");
+}
 
+function vacaFeliz() {
+  alert("A vaca ajuda na produção de leite e derivados! 🐄");
+}
+
+function frutasFelizes() {
+  alert("As frutas são importantes para uma alimentação saudável! 🍎🍌🍇🍓🍍");
+}
